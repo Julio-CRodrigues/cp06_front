16acaaf210e80d8d4eec07424e09ca183e4a9c3e
gudeaguiar <gudeaguiar@gmail.com>
2024-10-22 21:23:25 -0300

    Páginas de erro 404 e 500 adicionadas.

---
+++ cp06_front/src/app/500.tsx
@@ -0,0 +1,9 @@
+export default function Custom500() {
+    return (
+      <div className="bg-navy text-white flex items-center justify-center min-h-screen">
+        <h1 className="text-6xl">500</h1>
+        <p className="text-xl ml-4">Erro interno do servidor</p>
+      </div>
+    );
+  }
+  
---
+++ cp06_front/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <div className="bg-navy text-white flex items-center justify-center min-h-screen">
+        <h1 className="text-6xl">404</h1>
+        <p className="text-xl ml-4">Esta página não foi encontrada!</p>
+      </div>
+    );
+  }
+  
