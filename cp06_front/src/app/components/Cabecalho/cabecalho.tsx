751b8632240e7c7beb0d88eb2b790c96a94d6017
Julio-CRodrigues <juliocesarccr29@gmail.com>
2024-10-27 22:29:10 -0300

    Reformulado pagina sobre e cabecalho

--- cp06_front/src/app/components/Cabecalho/cabecalho.tsx
+++ cp06_front/src/app/components/Cabecalho/cabecalho.tsx
@@ -5,7 +5,7 @@ export default function Cabecalho() {
     <header className="bg-blue-800 text-white py-4 px-6 flex justify-between items-center">
       <h1 className="text-2xl font-bold">PORTFÓLIO FIAP</h1>
       <nav>
-        <Link href="/sobre" className="px-4 py-2 bg-pink-500 text-white rounded hover:bg-pink-600">
+        <Link href="/sobre" className="px-4 py-2 bg-pink-700 text-white rounded hover:bg-pink-500">
           Sobre o Projeto
         </Link>
       </nav>
--- cp06_front/src/app/sobre/page.tsx
+++ cp06_front/src/app/sobre/page.tsx
@@ -1,5 +1,11 @@
+import Cabecalho from "../components/Cabecalho/cabecalho";
+import Rodape from "../components/Rodape/rodape";
+import Link from "next/link";
+
 export default function Sobre() {
     return (
+      <> 
+      <Cabecalho />
       <div className="container mx-auto p-8">
         <h1 className="text-3xl font-bold mb-6">Sobre o Projeto</h1>
         <p>
@@ -8,6 +14,11 @@ export default function Sobre() {
           de apresentação de integrantes, métodos de avaliação e navegação entre
           diferentes páginas.
         </p>
+        <Link href="/" className="block mt-6 bg-pink-700 text-white text-center py-2 rounded hover:bg-pink-500">
+          Voltar para Home
+        </Link>
       </div>
+      <Rodape />
+    </>
     );
   }
